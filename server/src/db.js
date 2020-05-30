7dc57d3bd28dce94b6b3b39cd5ec20864a686e22
Mike Al Owe <mike.awie@yahoo.com>
2020-05-30 23:49:10 +0300

    stations and routes endpoints

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,12 @@
 import knex from 'knex';
 import knexPostgis from 'knex-postgis';
+import knexStringcase from 'knex-stringcase';
 const knexConfig = require('../knexfile');
 
 const environment = process.env.NODE_ENV || 'development';
 const connectionConfig = knexConfig[environment];
-
-const connection = knex(connectionConfig);
+const options = knexStringcase(connectionConfig);
+const connection = knex(options);
 
 export const gis = knexPostgis(connection);
 
