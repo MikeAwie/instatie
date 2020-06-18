3f6062def3afe1d40771cf79e80ace57743762eb
Mike Al Owe <mike.awie@yahoo.com>
2020-06-18 12:57:31 +0300

    docker tweaks

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'api',
       script: './src/index.js',
+      node_args: '-r esm',
       watch: true,
       ignore_watch: ['node_modules', 'docker-data'],
     },
--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ import './lib/cron';
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at port ${port}`);
 });
 
 process.on('uncaughtException', (error) => {
